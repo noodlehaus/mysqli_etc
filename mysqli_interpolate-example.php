5c71b0b8bab31ad4dfb2e1392123f988012558f4
noodlehaus <noodlehaus@gmail>
2015-03-21 00:05:45 +0800

    number of query args now drive interpolation, better docs

--- mysqli_interpolate-example.php
+++ mysqli_interpolate-example.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/minql.php';
+require __DIR__.'/mysqli_interpolate.php';
 
 $db = mysqli_connect('localhost', 'root', '', 'demo');
 
@@ -8,9 +8,11 @@ $s1 = mysqli_interpolate(
   'select * from users where username=? limit 1',
   'noodlehaus'
 );
+$s1->execute();
 var_dump($s1->get_result());
 $s1->free_result();
 
 $s2 = mysqli_interpolate($db, 'select * from users');
+$s2->execute();
 var_dump($s2->get_result());
 $s2->free_result();
