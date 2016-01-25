06acf92fcf4080bcadb5e67e943fbbbaece2508d
Jesus A. Domingo <jesdomingo@paypal.com>
2016-01-25 15:44:04 +0800

    removed mysqli_select_all()

--- mysqli_etc.php
+++ mysqli_etc.php
@@ -1,33 +1,27 @@
 <?php
 
-# @author Jesus A. Domingo <jesus.domingo@gmail.com>
 # @license MIT
 
-# Executes a select and returns a single row.
-function mysqli_select_one($db, $sql, ...$params) {
+# Executes an insert and returns the insert id if any.
+function mysqli_insert($db, $sql, ...$params) {
 
   $stmt = mysqli_interpolate($db, $sql, ...$params);
 
-  if (
-    !mysqli_stmt_execute($stmt) ||
-    false === ($result = mysqli_stmt_get_result($stmt))
-  ) {
+  if (!mysqli_stmt_execute($stmt)) {
     throw new mysqli_sql_exception(
       mysqli_stmt_error($stmt),
       mysqli_stmt_errno($stmt)
     );
   }
 
-  $row = mysqli_fetch_assoc($result);
-
-  mysqli_free_result($result);
+  $id = mysqli_insert_id($db);
   mysqli_stmt_close($stmt);
 
-  return $row;
+  return (string) $id;
 }
 
 # Executes a select and returns all resulting rows
-function mysqli_select_all($db, $sql, ...$params) {
+function mysqli_select($db, $sql, ...$params) {
 
   $stmt = mysqli_interpolate($db, $sql, ...$params);
 
@@ -49,7 +43,7 @@ function mysqli_select_all($db, $sql, ...$params) {
   return $rows;
 }
 
-# Executes an update query and returns affected row count.
+# Executes an update/delete query and returns affected row count.
 function mysqli_update($db, $sql, ...$params) {
 
   $stmt = mysqli_interpolate($db, $sql, ...$params);
@@ -65,24 +59,6 @@ function mysqli_update($db, $sql, ...$params) {
   mysqli_stmt_close($stmt);
 
   return $affected;
-}
-
-# Executes an insert and returns the insert id if any.
-function mysqli_insert($db, $sql, ...$params) {
-
-  $stmt = mysqli_interpolate($db, $sql, ...$params);
-
-  if (!mysqli_stmt_execute($stmt)) {
-    throw new mysqli_sql_exception(
-      mysqli_stmt_error($stmt),
-      mysqli_stmt_errno($stmt)
-    );
-  }
-
-  $id = mysqli_insert_id($db);
-  mysqli_stmt_close($stmt);
-
-  return (string) $id;
 }
 
 # Expansion of mysqli_prepare
